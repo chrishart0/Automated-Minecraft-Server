7e530ea99c91f02523d9304cd0d4bf50b08dda18
daniels1996 <daniel.ksingletary@gmail.com>
2020-08-20 08:11:15 -0400

    Alert Box

--- Front End/js/start_button_api.js
+++ Front End/js/start_button_api.js
@@ -2,8 +2,10 @@ const fetch = require("node-fetch");
 function updateVisitCount() {
     fetch('https://0tgloih8p9.execute-api.us-east-1.amazonaws.com/Prod/hello')
         .then(response => {
-            console.log(response)
-        return response.json()
-      })
-    }
+            return response.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+}
 updateVisitCount()
