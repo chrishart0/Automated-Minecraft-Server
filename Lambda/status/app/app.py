366677633d69d81b21f6dcfbc497dd996989d34b
daniels1996 <66973507+daniels1996@users.noreply.github.com>
2020-08-20 12:11:20 -0400

    Updated CORS

--- Lambda/status/app/app.py
+++ Lambda/status/app/app.py
@@ -50,6 +50,7 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        'headers': { "Access-Control-Allow-Origin": "*" },
         "body": json.dumps({
             "message": status
         }),
